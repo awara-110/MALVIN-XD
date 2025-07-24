8612aa0019e3e6439df9bb5f8b1d7fbc42279e65
awara-110 <awarameer0110@gmail.com>
2025-07-25 00:41:08 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~Y2smUT7S#dPf_6c-4_ESiRPj9xjfIiz8KznPbJwL-SahGSPOqIZ4 ",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
